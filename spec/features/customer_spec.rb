e3e2b18653cb6940ee6536431ba38d675d420cb6
junior <juniormathiias@gmail.com>
2024-04-03 22:43:35 -0300

    scenario for new customer, form and new migrate and models

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -19,5 +19,19 @@ RSpec.feature "Customers", type: :feature do
     expect(page).to  have_content("New Customer")
   end 
 
+  scenario 'Verify a new valid customer' do 
+    visit(new_customer_path)
+    customer_name = Faker::Name.name
+    fill_in('Name', with: customer_name)
+    fill_in('Email', with: Faker::Internet.email)
+    fill_in('Phone', with: Faker::PhoneNumber.phone_number)
+    attach_file('Profile Photo', "#{Rails.root}/spec/fixtures/avatar.png")
+    choose(option: ['S','N'].sample)
+
+    click_on('Register Customer')
+    expect(page).to  have_content("Customer Registered Successfully")
+    expect(Customer.last.name).to eq(customer_name)
+  end 
+
 
 end
