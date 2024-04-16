551eeb8c4ea7e24d646f1ab590f8740b9c9591d4
junior <juniormathiias@gmail.com>
2024-04-15 21:12:04 -0300

    fixed the bugs

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -82,8 +82,8 @@ RSpec.feature "Customers", type: :feature do
       avatar: "#{Rails.root}/spec/fixtures/avatar.png"
     )
     visit(customers_path)
-    find(:xpath, "/html/body/table/tbody/tr[1]/td[2]/a").click
-    expect(page).to  have_content("Listing Customers")
+    click_link('Show', href: customer_path(customer))
+    expect(page).to have_content("Listing Customers")
   end
 
   scenario 'Click on button Edit of index' do 
@@ -95,9 +95,9 @@ RSpec.feature "Customers", type: :feature do
       avatar: "#{Rails.root}/spec/fixtures/avatar.png"
     )
     visit(customers_path)
-    find(:xpath, "/html/body/table/tbody/tr[1]/td[3]/a").click
-    expect(page).to  have_content("Edit Customer")
-  end
+    click_link('Edit', href: edit_customer_path(customer))
+    expect(page).to have_content("Edit Customer")
+  end  
 
   scenario 'Delete a Customer', js: true do 
     customer = Customer.create!(
@@ -108,10 +108,11 @@ RSpec.feature "Customers", type: :feature do
       avatar: "#{Rails.root}/spec/fixtures/avatar.png"
     )
     visit(customers_path)
-    find(:xpath, "/html/body/table/tbody/tr[1]/td[4]/a").click
-    1.second
+    within("tbody tr:first-child") do
+      click_link('Delete')
+    end
     page.driver.browser.switch_to.alert.accept
-    expect(page).to  have_content("Customer Deleted Successfully")
+    expect(page).to have_content("Customer Deleted Successfully")
   end
 
 end
